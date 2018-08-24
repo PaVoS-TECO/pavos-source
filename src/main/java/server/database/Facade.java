af153cc05162db94ebfcc6ff0c2fc80eef04ed24
Oliver <32925177+olivermliu@users.noreply.github.com>
2018-08-24 17:59:36 +0200

    Database (#6)
    * Added functionality to add and get ObservationData objects from DB
    
    + Facade supports adding serialized and non-serialized ObservationData objects to DB
    + Facade supports getting a String value of a certain observedProperty from a certain cluster at a certain time

--- src/main/java/server/database/Facade.java
+++ src/main/java/server/database/Facade.java
@@ -1,9 +1,7 @@
 package server.database;
 
-import java.util.Set;
-
-import org.apache.kafka.common.metrics.Sensor;
-
+import server.transfer.data.ObservationData;
+import server.transfer.data.ObservationDataDeserializer;
 import web.grid.Grid;
 
 /**
@@ -11,33 +9,36 @@ import web.grid.Grid;
  */
 public class Facade {
 
+	private KafkaToStorageProcessor storageProcessor;
+	
     /**
      * Default constructor
      */
     public Facade() {
+    	// TODO set host by property list
+    	storageProcessor = new KafkaToStorageProcessor("localhost");
     }
-
-
-
-
-
+    
     /**
-     * Subscribes to the given KafkaStream, which contains ZoomLevel-specific data and initiates processing of its records.
-     * @param stream The stream to subscribe to.
+     * Add an ObservationData object to the storage solution.
+     * @param observationData The ObservationData object.
      */
-    public void subscribeToZoomLevelStream(KStream stream) {
-        // TODO implement here
+    public void addObservationData(ObservationData observationData) {
+    	storageProcessor.add(observationData);
     }
-
+    
     /**
-     * Fetches all sensors from the given cluster that observe the given ObservedProperty and returns an array of sensors.
-     * @param type The ObservationType of the requested sensors.
-     * @param id The ID of the cluster.
-     * @return An array of sensors.
+     * Add a byte array (which represents a serialized ObservationData object)
+     * to the storage solution.
+     * @param observationData The serialized ObservationData object.
      */
-    public Set<Sensor> getSensors(ObservationType type, ClusterID id) {
-        // TODO implement here
-        return null;
+    public void addObservationData(byte[] observationData) {
+    	ObservationDataDeserializer deserializer = new ObservationDataDeserializer();
+    	ObservationData obsDataObject = deserializer.deserialize(null, observationData);
+    	deserializer.close();
+    	if (obsDataObject != null) {
+    		addObservationData(obsDataObject);
+    	}
     }
 
     /**
@@ -47,9 +48,27 @@ public class Facade {
      * @param time The point in time.
      * @return A grid with the computed data.
      */
-    public Grid getGrid(ClusterID[] clusters, ZoomLevel zoom, Time time) {
+    public Grid getGrid(ClusterID[] clusters, ZoomLevel zoom, String timestamp) {
         // TODO implement here
         return null;
+    }
+    
+    /**
+     * Get the value of an observedProperty from a clusterID at or before the given timestamp.
+     * The returned value is guaranteed to come from an observation in the given cluster at or before
+     * the given timestamp (i.e. no values from the future).
+     * @param clusterID The cluster from which to get the value
+     * @param timestamp The time to check
+     * @param observedProperty The observedProperty needed
+     * @return The value to the observedProperty key. Returns {@code null} in any of the following cases:<br>
+     * - There is no entry for the cluster<br>
+     * - There is no entry for the cluster before or at the given timestamp<br>
+     * - There is no {@code observedProperty} key in the observations Map<br>
+     * - The value to the {@code observedProperty} key is literally {@code null}<br>
+     * - Any of the parameters is badly formatted (see logs)
+     */
+    public String getObservationData(String clusterID, String timestamp, String observedProperty) {
+    	return storageProcessor.get(clusterID, timestamp, observedProperty);
     }
 
 }
