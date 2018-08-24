5ffb85fd96ba7c91c952c4d7fb9a32cb85cacde7
Erik Wessel <Erik.Wessel@web.de>
2018-08-24 19:08:46 +0200

    [Core][Web] Servlet update for DB
    + split functionality: getLiveData(); getDatabaseData();
    + added parameter "steps" to request

--- src/main/java/server/core/web/DataServlet.java
+++ src/main/java/server/core/web/DataServlet.java
@@ -9,10 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
 import server.core.grid.GeoGrid;
 import server.core.grid.GeoGridManager;
 import server.core.grid.geojson.GeoJsonConverter;
 import server.core.grid.polygon.GeoPolygon;
+import server.database.Facade;
+import server.transfer.data.ObservationData;
+import server.transfer.sender.util.TimeUtil;
 
 public class DataServlet  extends HttpServlet {
 	
@@ -30,8 +36,65 @@ public class DataServlet  extends HttpServlet {
 		String gridID = req.getParameter("gridID");
 		String fusedClusterIDs = req.getParameter("clusterID");
 		String keyProperty = req.getParameter("property");
-		
+		String fusedTime = req.getParameter("time");
 		String[] clusterIDs = fusedClusterIDs.split(",");
+		String[] time = fusedTime.split(",");
+		String stepsString = req.getParameter("steps");
+		
+		String result = null;
+		if (time[0] == null) {
+			result = getLiveData(gridID, keyProperty, clusterIDs, req, res);
+		} else {
+			result = getDatabaseData(gridID, keyProperty, clusterIDs, time, stepsString, req, res);
+		}
+		
+		res.setContentType("application/json");
+		res.setCharacterEncoding("UTF-8");
+	    res.getWriter().write(result);
+	}
+	
+	private String getDatabaseData(String gridID, String keyProperty, String[] clusterIDs, String[] time, 
+			String stepsString, HttpServletRequest req, HttpServletResponse res) throws ServletException {
+		if (time.length == 1) {
+			Facade database = new Facade();
+			Collection<ObservationData> observations = new HashSet<>();
+			for (String clusterID : clusterIDs) {
+				String val = database.getObservationData(clusterID, time[0], keyProperty);
+				ObservationData data = new ObservationData();
+				data.clusterID = clusterID;
+				data.observationDate = time[0];
+				data.observations.put(keyProperty, val);
+				observations.add(data);
+			}
+			GeoGridManager manager = GeoGridManager.getInstance();
+			return GeoJsonConverter.convertObservations(observations, keyProperty, manager.getGrid(gridID));
+		} else if (time.length == 2) {
+			DateTime dt1 = TimeUtil.getUTCDateTime(time[0]).toDateTime(DateTimeZone.UTC);
+			DateTime dt2 = TimeUtil.getUTCDateTime(time[1]).toDateTime(DateTimeZone.UTC);
+			long dt1Millis = dt1.getMillis();
+			long dt2Millis = dt2.getMillis();
+			long minMillis = Math.min(dt1Millis, dt2Millis);
+			int steps = Integer.parseInt(stepsString);
+			long diff = Math.abs(dt1Millis - dt2Millis) / steps;
+			
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < steps; i++) {
+				long currentMillis = minMillis + (long) steps * diff;
+				DateTime dtCurrent = new DateTime(currentMillis, DateTimeZone.UTC);
+				String[] currentTimestamp = new String[1];
+				currentTimestamp[0] = TimeUtil.getUTCDateTimeString(dtCurrent.toLocalDateTime());
+				builder.append(getDatabaseData(gridID, keyProperty, clusterIDs, time, stepsString, req, res));
+				if (i < steps - 1) {
+					builder.append(", ");
+				}
+			}
+			
+		}
+		throw new ServletException("Time format unacceptable.");
+	}
+	
+	private String getLiveData(String gridID, String keyProperty, String[] clusterIDs, 
+			HttpServletRequest req, HttpServletResponse res) throws ServletException {
 		Collection<GeoPolygon> polygons = new HashSet<>();
 		final GeoGrid grid = getGrid(gridID);
 		for (int i = 0; i < clusterIDs.length; i++) {
@@ -40,9 +103,7 @@ public class DataServlet  extends HttpServlet {
 				polygons.add(polygon);
 			}
 		}
-		res.setContentType("application/json");
-		res.setCharacterEncoding("UTF-8");
-	    res.getWriter().write(GeoJsonConverter.convertPolygons(polygons, keyProperty));
+		return GeoJsonConverter.convertPolygons(polygons, keyProperty);
 	}
 	
 	private GeoGrid getGrid(String gridID) throws ServletException {
